39012b8f279c37cfe9245549ca3867d72f64a65e
Bhumisjm <bhumisjm@gmail.com>
2016-02-23 11:53:30 +0530

    commit to test

--- NewGitSample/ViewController.swift
+++ NewGitSample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("1231232")
+        print("6327462784")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
